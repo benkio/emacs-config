b4194aa1935ddfd5892fb5a1a439bb4d170a6a5b
Enrico Benini <ebenini@mdsol.com>
2019-07-05 16:54:43 +0100

    Add the browse-kill-ring package

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 		"flycheck.org"
 		"silverSearcher.org"
 		"company.org"
+		"browseKillRing.org"
 		"programminglanguages.org"
 		"functions/formatting.org"
 		"functions/filenamePathToClipboard.org"
@@ -44,20 +45,3 @@
 (set 'configs (mapcar (lambda (x) (concat configPrefix x)) configs))
 (require 'org)
 (mapc (lambda (x) (org-babel-load-file (expand-file-name x))) configs)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
- '(package-selected-packages
-   (quote
-    (super-save gruber-darker-theme ag exec-path-from-shell monokai-theme projectile use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
