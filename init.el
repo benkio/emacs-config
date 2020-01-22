782613d36ee2411cce64ee092d5d6fef8dcc6ad0
Enrico Benini <ebenini@mdsol.com>
2020-01-22 15:17:25 +0000

    Remove Init generated variables. Remove mistake in scala.org

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;;;; Added by Package.el.  This must come before configurations of
+;;;; installed packages.  Don't delete this line.  If you don't want it,
+;;;; just comment it out by adding a semicolon to the start of the line.
+;;;; You may delete these explanatory comments.
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/orgConfig/packages.org")
@@ -54,17 +54,3 @@
 (set 'configs (mapcar (lambda (x) (concat configPrefix x)) configs))
 (require 'org)
 (mapc (lambda (x) (org-babel-load-file (expand-file-name x))) configs)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auctex undo-tree csv-mode yasnippet-snippets yaml-mode writegood-mode which-key use-package super-save scala-mode sbt-mode restclient py-yapf py-autopep8 ox-reveal ox-gfm monokai-theme magit jedi iedit htmlize hindent haskell-mode git-link flymake-haskell-multi flymake-cursor flycheck-pycheckers expand-region exec-path-from-shell elpy ein crux counsel-projectile browse-kill-ring ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
