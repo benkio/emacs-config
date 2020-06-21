412f0a1060921123a66c7b8ee51cb5f543aabb62
Enrico Benini <benkio89@gmail.com>
2020-06-21 17:13:07 +0100

    Change color schemes to dark, 3 colors

--- init.el
+++ init.el
@@ -59,17 +59,3 @@
 (org-babel-load-file "~/.emacs.d/orgConfig/functions/ffmpeg.org"                    )
 (org-babel-load-file "~/.emacs.d/orgConfig/functions/youtube.org"                   )
 (org-babel-load-file "~/.emacs.d/orgConfig/undotree.org"                            )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auctex yasnippet-snippets use-ttf use-package git-link))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(region ((t (:inherit nil :background "purple4")))))
