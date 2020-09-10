b3ce01b90f66b922b7fc48f843c80af8729977f1
Enrico Benini <ebenini@mdsol.com>
2020-09-10 14:59:24 +0100

    Remove drag stuff in favor of DIY version ;)

--- init.el
+++ init.el
@@ -42,7 +42,6 @@
 (org-babel-load-file "~/.emacs.d/orgConfig/silverSearcher.org"                      )
 (org-babel-load-file "~/.emacs.d/orgConfig/autoComplete.org"                        )
 (org-babel-load-file "~/.emacs.d/orgConfig/restclient.org"                          )
-(org-babel-load-file "~/.emacs.d/orgConfig/dragStuff.org"                           )
 (org-babel-load-file "~/.emacs.d/orgConfig/idleHighlightMode.org"                   )
 (org-babel-load-file "~/.emacs.d/orgConfig/browseKillRing.org"                      )
 (org-babel-load-file "~/.emacs.d/orgConfig/programminglanguages.org"                )
