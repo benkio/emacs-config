50421829e110792a62be0d23514f813900abc244
Enrico Benini <ebenini@mdsol.com>
2021-04-18 13:55:40 +0100

    Flatten all the config

--- init.el
+++ init.el
@@ -6,31 +6,3 @@
 (server-start)
 
 (org-babel-load-file "~/.emacs.d/config.org")
-;; (org-babel-load-file "~/.emacs.d/orgConfig/yas.org"                                 )
-
-;; (org-babel-load-file "~/.emacs.d/orgConfig/expandRegion.org"                        )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/lilypond.org"                            )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/recentf.org"                             )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/crux.org"                                )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/supersave.org"                           )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/flycheck.org"                            )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/company.org"                             )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/restclient.org"                          )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/idleHighlightMode.org"                   )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/browseKillRing.org"                      )
-;; (org-babel-load-file "~/.emacs.d/orgConfig/programminglanguages.org"                )
-
-;; (org-babel-load-file "~/.emacs.d/orgConfig/undotree.org"                            )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(exec-path-from-shell yaml-mode which-key wgrep use-ttf use-package unicode-fonts restclient projectile ox-twbs ox-reveal ox-jira ox-gfm magit iedit htmlize google-this golden-ratio git-link csv-mode ag)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(whitespace-space ((t (:background "grey5" :foreground "grey15")))))
