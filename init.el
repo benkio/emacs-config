f28c960b6687a22f56ed5940a2c8c396398765ae
Enrico Benini <benkio89@gmail.com>
2021-11-28 16:20:14 +0000

    require server

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 ;;;; just comment it out by adding a semicolon to the start of the line.
 ;;;; You may delete these explanatory comments.
 (package-initialize)
+(require 'server)
 (unless (server-running-p)
     (server-start))
 
