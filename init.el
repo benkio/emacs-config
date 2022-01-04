b3894869267cd475142439a83fbb62a5043f6177
Enrico Benini <benkio89@gmail.com>
2022-01-04 11:40:47 +0000

    prioritaization of fonts, not by system

--- init.el
+++ init.el
@@ -8,16 +8,3 @@
     (server-start))
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(rg marginalia selectrum-prescient selectrum yasnippet-snippets yaml-mode which-key wgrep-ag use-package undo-tree typescript-mode super-save simple-httpd scala-mode sbt-mode restclient ox-twbs ox-reveal ox-jira ox-gfm ormolu nix-mode markdown-toc magit lorem-ipsum json-mode ivy indent-guide iedit idle-highlight-mode htmlize google-this golden-ratio git-link flymake-haskell-multi flymake-cursor flycheck expand-region exec-path-from-shell elm-mode editorconfig dotnet dedicated csv-mode csharp-mode crux company-ghci browse-kill-ring auctex ag)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
