f2ab6d66992df33fc567f6ccc4a0f9a6df2b8d80
Enrico Benini <benkio89@gmail.com>
2022-05-30 20:03:32 +0100

    selectrum + marginalia => IComplete and FIDO for emacs 28. Remove Flycheck and Flymake

--- init.el
+++ init.el
@@ -8,3 +8,16 @@
     (server-start))
 
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(sqlformat yasnippet-snippets yaml-mode which-key wgrep-ag use-package undo-tree typescript-mode super-save simple-httpd selectrum-prescient scala-mode sbt-mode rg restclient ox-twbs ox-reveal ox-jira ox-gfm ormolu nix-mode markdown-toc marginalia magit lorem-ipsum json-mode ivy indent-guide iedit idle-highlight-mode htmlize highlight google-this golden-ratio git-link flymake-haskell-multi flymake-cursor flycheck expand-region exec-path-from-shell elm-mode editorconfig dotnet dedicated csv-mode csharp-mode crux company-ghci browse-kill-ring auctex ag)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
