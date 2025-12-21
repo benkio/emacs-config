694c94278a4dbf6b16e72baa3da2d43449e7e7d6
Enrico Benini <benkio89@gmail.com>
2025-12-21 15:53:58 +0100

    Compile snippets and trisit grammar

--- snippets/sql-mode/.yas-compiled-snippets.el
+++ snippets/sql-mode/.yas-compiled-snippets.el
@@ -4,12 +4,12 @@
 (yas-define-snippets 'sql-mode
                      '(("update" "UPDATE $1 SET $2 = $3 WHERE $4; $0"
                         "update" nil nil nil
-                        "/home/benkio/.emacs.d/snippets/sql-mode/update"
+                        "/Users/benkio/.emacs.d/snippets/sql-mode/update"
                         nil nil)
                        ("delete" "DELETE FROM $1 WHERE $2; $0"
                         "delete" nil nil nil
-                        "/home/benkio/.emacs.d/snippets/sql-mode/delete"
+                        "/Users/benkio/.emacs.d/snippets/sql-mode/delete"
                         nil nil)))
 
 
-;;; Do not edit! File generated at Thu Aug 28 10:13:59 2025
+;;; Do not edit! File generated at Sat Dec 13 12:05:07 2025
--- snippets/typescript-ts-mode/.yas-compiled-snippets.el
+++ snippets/typescript-ts-mode/.yas-compiled-snippets.el
@@ -5,8 +5,8 @@
                      '(("test"
                         "describe('$1', () => {\n  it('$2', () => {\n    $0\n  });\n});"
                         "test" nil nil nil
-                        "/home/benkio/.emacs.d/snippets/typescript-ts-mode/test"
+                        "/Users/benkio/.emacs.d/snippets/typescript-ts-mode/test"
                         nil nil)))
 
 
-;;; Do not edit! File generated at Thu Aug 28 10:13:59 2025
+;;; Do not edit! File generated at Sat Dec 13 12:05:07 2025
